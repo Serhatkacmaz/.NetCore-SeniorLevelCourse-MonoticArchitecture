cdc075c2a399ee549ec626003e3c90f5ccb827df
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-05-17 00:18:46 +0300

    lesson 1 by skacmaz

--- src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Application.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
 
 namespace Persistence
 {
     public static class PersistenceServiceRegistration
     {
-        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                     IConfiguration configuration)
         {
-            return services;
+            services.AddDbContext<BaseDbContext>(options =>
+                                                     options.UseSqlServer(
+                                                         configuration.GetConnectionString("RentACarCampConnectionString")));
+            services.AddScoped<IBrandRepository, BrandRepository>();
 
+            return services;
         }
     }
 }
