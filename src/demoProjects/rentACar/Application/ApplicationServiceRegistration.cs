87830ea0b9ed386227cc3235176b8a7ddddd7eb6
Serhat Kaçmaz <serhatkacmaz3@gmail.com>
2023-05-16 23:33:10 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+
+
+            return services;
+
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+
+        }
+    }
+}
